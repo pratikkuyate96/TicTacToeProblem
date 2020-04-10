2327c6e4a90d9d0882bf2e1c4091c2dbc635d0a2
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-10 17:15:04 +0530

    ADDED: Show code to reset the board

---
+++ TicTacToe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+echo "..........WELCOME TO TIC-TAC-TOE.................."
+
+declare -a board
+board=( 1 2 3 4 5 6 7 8 9 )
+
+function displayBoard() {
+	for (( row=0; row<=6; row=row+3 ))
+	do
+		echo "${board[$row]} | ${board[$row+1]} | ${board[$row+2]}"
+		if [ $row -lt 6 ]
+		then
+		echo "----------"
+		fi
+	done
+}
+
+displayBoard
